405e9e2c83920213e6348ae6641f572303dece3c
Ambuj <kumarambuj1997@gmail.com>
2024-10-20 17:38:00 +0530

    UPDATED

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import mixpanel from 'mixpanel-browser';
 
 // Initialize Mixpanel with your token
-mixpanel.init('240a4a5ced6323f58ef8954774c41e6c', { debug: true });
+//mixpanel.init('240a4a5ced6323f58ef8954774c41e6c', { debug: true });
+
+mixpanel.init("240a4a5ced6323f58ef8954774c41e6c", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
 
 // Track a page load event
-mixpanel.track('Page Loaded');
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
